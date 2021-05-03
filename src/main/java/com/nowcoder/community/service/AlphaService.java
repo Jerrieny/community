9417406445c45a7a0f8e2e8cf5a8537d3a769efe
scy <scy97222@126.com>
2021-05-03 18:06:11 +0800

    First Commit

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+//@Scope("prototype")
+//默认单例
+public class AlphaService {
+
+    @Autowired
+    private AlphaDao alphaDao;
+
+    public AlphaService(){
+        System.out.println("实例化");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化");
+    }
+    @PreDestroy
+    public void destory(){
+        System.out.println("销毁");
+    }
+
+    public String find(){
+        return alphaDao.select();
+    }
+
+}
