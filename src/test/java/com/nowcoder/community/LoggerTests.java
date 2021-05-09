ee8835f94799e0aaf34a450bb9cca855b1feb13f
scy <scy97222@126.com>
2021-05-09 20:58:48 +0800

    3rd Commit

--- src/test/java/com/nowcoder/community/LoggerTests.java
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringBootVersion;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.SpringVersion;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
